a1dd2809899f4cc4c585507681ebcc9f26c384cf
Iraj <1mohtashamiraj@gmail.com>
2022-10-30 16:44:13 +0330

    Vehicle Camera spring arm added features

--- Source/ModularMovement/Private/Utility/VehicleSpringArm.cpp
+++ Source/ModularMovement/Private/Utility/VehicleSpringArm.cpp
@@ -1,86 +1,398 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "VehicleSpringArm.h"
-
-
-#include "Components/MeshComponent.h"
-#include "GameFramework/Actor.h"
-#include "GameFramework/Controller.h"
+#include "GameFramework/Pawn.h"
+#include "CollisionQueryParams.h"
+#include "WorldCollision.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+#include "ModularMovementComponent.h"
 #include "Kismet/KismetMathLibrary.h"
 
 
+//////////////////////////////////////////////////////////////////////////
+// UVehicleSpringArm
 
+const FName UVehicleSpringArm::SocketName(TEXT("SpringEndpoint"));
 
- 
-void UVehicleSpringArm::TickComponent(float DeltaTime, ELevelTick TickType,
-                                      FActorComponentTickFunction* ThisTickFunction)
+UVehicleSpringArm::UVehicleSpringArm(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
 {
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.TickGroup = TG_PostPhysics;
 
+	bAutoActivate = true;
+	bTickInEditor = true;
+	bUsePawnControlRotation = false;
+	bDoCollisionTest = true;
 
-	
-	
-	if(CoolDown<0.f)
+	bInheritPitch = true;
+	bInheritYaw = true;
+	bInheritRoll = true;
+
+	TargetArmLength = 300.0f;
+	ProbeSize = 12.0f;
+	ProbeChannel = ECC_Camera;
+
+	RelativeSocketRotation = FQuat::Identity;
+
+	bUseCameraLagSubstepping = true;
+	bEnableCameraLag = true;
+	bEnableCameraRotationLag = true;
+	CameraLagZSpeed = 5;
+	CameraRotationLagSpeed = 2.f;
+	CameraLagMaxDistance = 100.f;
+	bUsePawnControlRotation = true;
+	TargetArmLength = 700.f;
+	CameraLagMaxTimeStep = 1.f / 60.f;
+	CameraLagMaxDistance = 0.f;
+
+	UnfixedCameraPosition = FVector::ZeroVector;
+}
+
+FRotator UVehicleSpringArm::GetDesiredRotation() const
+{
+	return GetComponentRotation();
+}
+
+FRotator UVehicleSpringArm::GetTargetRotation() const
+{
+	FRotator DesiredRot = GetDesiredRotation();
+
+	if (bUsePawnControlRotation)
 	{
 		if (APawn* OwningPawn = Cast<APawn>(GetOwner()))
 		{
-			const FRotator CurrentRot=bUsePawnControlRotation?	OwningPawn->GetViewRotation():GetComponentRotation();
-			UMeshComponent* Mesh=Cast<UMeshComponent>(GetOwner()->GetRootComponent());
-			if(Mesh)
+			const FRotator PawnViewRotation = OwningPawn->GetViewRotation();
+			if (DesiredRot != PawnViewRotation)
 			{
-				const FVector Velocity=Mesh->GetPhysicsLinearVelocity()*FVector(1,1,0);
-				if(Velocity.Size()>MinSpeedRange)
+				DesiredRot = PawnViewRotation;
+			}
+		}
+	}
+
+	// If inheriting rotation, check options for which components to inherit
+	if (!IsUsingAbsoluteRotation())
+	{
+		const FRotator LocalRelativeRotation = GetRelativeRotation();
+		if (!bInheritPitch)
+		{
+			DesiredRot.Pitch = LocalRelativeRotation.Pitch;
+		}
+
+		if (!bInheritYaw)
+		{
+			DesiredRot.Yaw = LocalRelativeRotation.Yaw;
+		}
+
+		if (!bInheritRoll)
+		{
+			DesiredRot.Roll = LocalRelativeRotation.Roll;
+		}
+	}
+
+	return DesiredRot;
+}
+
+void UVehicleSpringArm::UpdateDesiredArmLocation(bool bDoTrace, bool bDoLocationLag, bool bDoRotationLag,
+                                                 float DeltaTime)
+{
+	FRotator DesiredRot = GetTargetRotation();
+
+	// Apply 'lag' to rotation if desired
+	if (bDoRotationLag && CurrentCooldown <= 0.f)
+	{
+		if (bUseCameraLagSubstepping && DeltaTime > CameraLagMaxTimeStep && CameraRotationLagSpeed > 0.f)
+		{
+			const FRotator ArmRotStep = (DesiredRot - PreviousDesiredRot).GetNormalized() * (1.f / DeltaTime);
+			FRotator LerpTarget = PreviousDesiredRot;
+			float RemainingTime = DeltaTime;
+			while (RemainingTime > KINDA_SMALL_NUMBER)
+			{
+				const float LerpAmount = FMath::Min(CameraLagMaxTimeStep, RemainingTime);
+				LerpTarget += ArmRotStep * LerpAmount;
+				RemainingTime -= LerpAmount;
+
+				DesiredRot = FRotator(FMath::QInterpTo(FQuat(PreviousDesiredRot), FQuat(LerpTarget), LerpAmount,
+				                                       CameraRotationLagSpeed));
+				PreviousDesiredRot = DesiredRot;
+			}
+		}
+		else
+		{
+			DesiredRot = FRotator(FMath::QInterpTo(FQuat(PreviousDesiredRot), FQuat(DesiredRot), DeltaTime,
+			                                       CameraRotationLagSpeed));
+		}
+	}
+	PreviousDesiredRot = DesiredRot;
+
+	// Get the spring arm 'origin', the target we want to look at
+	FVector ArmOrigin = GetComponentLocation() + TargetOffset;
+	// We lag the target, not the actual camera position, so rotating the camera around does not have lag
+	FVector DesiredLoc = ArmOrigin;
+	if (bDoLocationLag)
+	{
+		if (bUseCameraLagSubstepping && DeltaTime > CameraLagMaxTimeStep && CameraLagZSpeed > 0.f)
+		{
+			const FVector ArmMovementStep = (DesiredLoc - PreviousDesiredLoc) * (1.f / DeltaTime);
+			FVector LerpTarget = PreviousDesiredLoc;
+
+			float RemainingTime = DeltaTime;
+			while (RemainingTime > KINDA_SMALL_NUMBER)
+			{
+				const float LerpAmount = FMath::Min(CameraLagMaxTimeStep, RemainingTime);
+				LerpTarget += ArmMovementStep * LerpAmount;
+				RemainingTime -= LerpAmount;
+
+				DesiredLoc = FMath::VInterpTo(PreviousDesiredLoc, LerpTarget, LerpAmount, CameraLagZSpeed);
+				PreviousDesiredLoc = DesiredLoc;
+			}
+		}
+		else
+		{
+			DesiredLoc = FMath::VInterpTo(PreviousDesiredLoc, DesiredLoc, DeltaTime, CameraLagZSpeed);
+		}
+
+		// Clamp distance if requested
+		bool bClampedDist = false;
+		if (CameraLagMaxDistance > 0.f)
+		{
+			const FVector FromOrigin = DesiredLoc - ArmOrigin;
+			if (FromOrigin.SizeSquared() > FMath::Square(CameraLagMaxDistance))
+			{
+				DesiredLoc = ArmOrigin + FromOrigin.GetClampedToMaxSize(CameraLagMaxDistance);
+				bClampedDist = true;
+			}
+		}
+
+#if !(UE_BUILD_SHIPPING || UE_BUILD_TEST)
+		if (bDrawDebugLagMarkers)
+		{
+			DrawDebugSphere(GetWorld(), ArmOrigin, 5.f, 8, FColor::Green);
+			DrawDebugSphere(GetWorld(), DesiredLoc, 5.f, 8, FColor::Yellow);
+
+			const FVector ToOrigin = ArmOrigin - DesiredLoc;
+			DrawDebugDirectionalArrow(GetWorld(), DesiredLoc, DesiredLoc + ToOrigin * 0.5f, 7.5f,
+			                          bClampedDist ? FColor::Red : FColor::Green);
+			DrawDebugDirectionalArrow(GetWorld(), DesiredLoc + ToOrigin * 0.5f, ArmOrigin, 7.5f,
+			                          bClampedDist ? FColor::Red : FColor::Green);
+		}
+#endif
+	}
+	DesiredLoc.X = ArmOrigin.X;
+	DesiredLoc.Y = ArmOrigin.Y;
+
+	PreviousArmOrigin = ArmOrigin;
+	PreviousDesiredLoc = DesiredLoc;
+
+	// Now offset camera position back along our rotation
+	DesiredLoc -= DesiredRot.Vector() * CurrentArmLen;
+	// Add socket offset in local space
+	DesiredLoc += FRotationMatrix(DesiredRot).TransformVector(SocketOffset);
+
+	// Do a sweep to ensure we are not penetrating the world
+	FVector ResultLoc;
+	if (bDoTrace && (CurrentArmLen != 0.0f))
+	{
+		bIsCameraFixed = true;
+		FCollisionQueryParams QueryParams(SCENE_QUERY_STAT(SpringArm), false, GetOwner());
+
+		FHitResult Result;
+		GetWorld()->SweepSingleByChannel(Result, ArmOrigin, DesiredLoc, FQuat::Identity, ProbeChannel,
+		                                 FCollisionShape::MakeSphere(ProbeSize), QueryParams);
+
+		UnfixedCameraPosition = DesiredLoc;
+
+		ResultLoc = BlendLocations(DesiredLoc, Result.Location, Result.bBlockingHit, DeltaTime);
+
+		if (ResultLoc == DesiredLoc)
+		{
+			bIsCameraFixed = false;
+		}
+	}
+	else
+	{
+		ResultLoc = DesiredLoc;
+		bIsCameraFixed = false;
+		UnfixedCameraPosition = ResultLoc;
+	}
+
+	// Form a transform for new world transform for camera
+	FTransform WorldCamTM(DesiredRot, ResultLoc);
+	// Convert to relative to component
+	FTransform RelCamTM = WorldCamTM.GetRelativeTransform(GetComponentTransform());
+
+	// Update socket location/rotation
+	RelativeSocketLocation = RelCamTM.GetLocation();
+	RelativeSocketRotation = RelCamTM.GetRotation();
+
+	UpdateChildTransforms();
+}
+
+FVector UVehicleSpringArm::BlendLocations(const FVector& DesiredArmLocation, const FVector& TraceHitLocation,
+                                          bool bHitSomething, float DeltaTime)
+{
+	return bHitSomething ? TraceHitLocation : DesiredArmLocation;
+}
+
+void UVehicleSpringArm::ApplyWorldOffset(const FVector& InOffset, bool bWorldShift)
+{
+	Super::ApplyWorldOffset(InOffset, bWorldShift);
+	PreviousDesiredLoc += InOffset;
+	PreviousArmOrigin += InOffset;
+}
+
+void UVehicleSpringArm::OnRegister()
+{
+	Super::OnRegister();
+
+	// enforce reasonable limits to avoid potential div-by-zero
+	CameraLagMaxTimeStep = FMath::Max(CameraLagMaxTimeStep, 1.f / 200.f);
+	CameraLagZSpeed = FMath::Max(CameraLagZSpeed, 0.f);
+
+	// Set initial location (without lag).
+	UpdateDesiredArmLocation(false, false, false, 0.f);
+}
+
+void UVehicleSpringArm::PostLoad()
+{
+	Super::PostLoad();
+}
+
+void UVehicleSpringArm::TickComponent(float DeltaTime, enum ELevelTick TickType,
+                                      FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+
+	if (APawn* OwningPawn = Cast<APawn>(GetOwner()))
+	{
+		UModularMovementComponent* MC = Cast<UModularMovementComponent>(OwningPawn->GetMovementComponent());
+		if (MC)
+		{
+			if (MC->GetNumberOfDriveWheelsTouchingGround() == 0)
+			{
+				AirborneTime += DeltaTime;
+			}
+			else
+			{
+				if (AirborneTime > MinAirborneTimeForCameraShake)
 				{
-					const float Interpolation=UKismetMathLibrary::MapRangeClamped(Velocity.Size(),MinSpeedRange,MaxSpeedRange,0,1)*InterpolationStrength;
-					FRotator Result=UKismetMathLibrary::RInterpTo_Constant(CurrentRot,Velocity.Rotation(),DeltaTime,Interpolation);
-					if(IgnorePitch)
+					//play shake
+
+					if (OwningPawn->GetController() && OwningPawn->GetController()->IsPlayerController())
 					{
-						Result.Pitch=CurrentRot.Pitch;
+						APlayerController* PC = Cast<APlayerController>(OwningPawn->GetController());
+						PC->ClientStartCameraShake(CameraShake);
 					}
-					if(bUsePawnControlRotation)
+				}
+				AirborneTime = 0.f;
+			}
+		}
+		const FRotator CurrentRot = bUsePawnControlRotation ? OwningPawn->GetViewRotation() : GetComponentRotation();
+		UMeshComponent* Mesh = Cast<UMeshComponent>(GetOwner()->GetRootComponent());
+		if (Mesh)
+		{
+			const FVector Velocity = Mesh->GetPhysicsLinearVelocity() * FVector(1, 1, 0);
+
+			if (CurrentCooldown > 0.f)
+			{
+				CurrentCooldown -= DeltaTime;
+			}
+			else
+			{
+				//orient camera towards the speed vector
+
+
+				if (Velocity.Size() > AutoCorrectMinSpeedRange)
+				{
+					const float Interpolation = UKismetMathLibrary::MapRangeClamped(
+							Velocity.Size(), AutoCorrectMinSpeedRange, AutoCorrectMaxSpeedRange, 0, 1) *
+						AutoCorrectInterpolationStrength;
+					FRotator Result = UKismetMathLibrary::RInterpTo(CurrentRot, Velocity.Rotation(), DeltaTime,
+					                                                Interpolation);
+					if (IgnorePitch)
+					{
+						Result.Pitch = CurrentRot.Pitch;
+					}
+					if (bUsePawnControlRotation)
 					{
 						OwningPawn->GetController()->SetControlRotation(Result);
-					}else
+					}
+					else
 					{
 						SetWorldRotation(Result);
 					}
-			
 				}
-		
+				//Find lag amount by acceleration
+				if (bEnableCameraLag)
+				{
+					const float Acceleration = ((Velocity * FVector(1, 1, 0)).Size() - PreviousSpeed) / DeltaTime;
+					const float InterpolationTarget = UKismetMathLibrary::MapRangeClamped(
+						Acceleration, MinAcceleration, MaxAcceleration, TargetArmLength - MaxArmLenChange,
+						TargetArmLength + MaxArmLenChange);
+					CurrentArmLen = UKismetMathLibrary::FInterpTo(CurrentArmLen, InterpolationTarget, DeltaTime,
+					                                              ArmLenInterpolationSpeed);
+				}
 			}
 
-		
+			PreviousSpeed = (Velocity * FVector(1, 1, 0)).Size();
 		}
-	}else
-	{
-		CoolDown-=DeltaTime;
 	}
-	
-	//Arm Len
-	if (APawn* OwningPawn = Cast<APawn>(GetOwner()))
+
+
+	UpdateDesiredArmLocation(bDoCollisionTest, bEnableCameraLag, bEnableCameraRotationLag, DeltaTime);
+}
+
+FTransform UVehicleSpringArm::GetSocketTransform(FName InSocketName, ERelativeTransformSpace TransformSpace) const
+{
+	FTransform RelativeTransform(RelativeSocketRotation, RelativeSocketLocation);
+
+	switch (TransformSpace)
 	{
-		UMeshComponent* Mesh=Cast<UMeshComponent>(GetOwner()->GetRootComponent());
-		if(Mesh)
+	case RTS_World:
 		{
-			const FVector Velocity= Mesh->GetPhysicsLinearVelocity();
-			const FVector SpeedDiff=	Velocity-PreviousVelocity;
-			
-		
-
-
-
-			TargetArmLength=	UKismetMathLibrary::FInterpTo_Constant(TargetArmLength,	UKismetMathLibrary::MapRangeClamped(UKismetMathLibrary::Dot_VectorVector(SpeedDiff,Mesh->GetForwardVector()),MinAccel,MaxAccel,MinArmLen,MaxArmLen),DeltaTime,ArmLenAnimSpeed);
-
-			PreviousVelocity=Velocity;
+			return RelativeTransform * GetComponentTransform();
+			break;
 		}
-		
+	case RTS_Actor:
+		{
+			if (const AActor* Actor = GetOwner())
+			{
+				FTransform SocketTransform = RelativeTransform * GetComponentTransform();
+				return SocketTransform.GetRelativeTransform(Actor->GetTransform());
+			}
+			break;
+		}
+	case RTS_Component:
+		{
+			return RelativeTransform;
+		}
 	}
-		
-	
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	return RelativeTransform;
+}
+
+bool UVehicleSpringArm::HasAnySockets() const
+{
+	return true;
+}
+
+void UVehicleSpringArm::QuerySupportedSockets(TArray<FComponentSocketDescription>& OutSockets) const
+{
+	new(OutSockets) FComponentSocketDescription(SocketName, EComponentSocketType::Socket);
+}
+
+FVector UVehicleSpringArm::GetUnfixedCameraPosition() const
+{
+	return UnfixedCameraPosition;
+}
+
+bool UVehicleSpringArm::IsCollisionFixApplied() const
+{
+	return bIsCameraFixed;
 }
 
 void UVehicleSpringArm::SetCooldown(float In)
 {
-	CoolDown=In;
+	CurrentCooldown = In;
 }
--- Source/ModularMovement/Public/Utility/VehicleSpringArm.h
+++ Source/ModularMovement/Public/Utility/VehicleSpringArm.h
@@ -1,48 +1,229 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/SpringArmComponent.h"
+#include "UObject/ObjectMacros.h"
+#include "Engine/EngineTypes.h"
+#include "Components/SceneComponent.h"
 #include "VehicleSpringArm.generated.h"
 
 /**
- * 
+This spring arm only lags on Z axis . Has ability to play shakes on hitting the ground . Automatically rotates to direction that vehicle is moving with support to manually
+*Rotate the camera . Use setcooldown to allow user to manually move the cam . once cooldown is finished camera will auto orient again .
  */
-UCLASS(ClassGroup=Camera, meta=(BlueprintSpawnableComponent), hideCategories=(Mobility))
-class MODULARMOVEMENT_API UVehicleSpringArm : public USpringArmComponent
-{
-	GENERATED_BODY()
-	
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-	public:
-	//Minimum speed to start interpolating CM/s
-	UPROPERTY(EditAnywhere)
-	float MinSpeedRange=5;
-	//Max speed to start interpolating CM/s
-	UPROPERTY(EditAnywhere)
-	float MaxSpeedRange=500;
-	//Interplolation Scale 
-	UPROPERTY(EditAnywhere)
-	float InterpolationStrength=120;
-	UPROPERTY(EditAnywhere)
-	bool IgnorePitch=true;
 
-	UPROPERTY(EditAnywhere)
-	float MinArmLen=450;
-	UPROPERTY(EditAnywhere)
-	float MaxArmLen=550;
-	UPROPERTY(EditAnywhere)
-	float MaxAccel=10;
-	UPROPERTY(EditAnywhere)
-	float MinAccel=-10;
-	UPROPERTY(EditAnywhere)
-	float ArmLenAnimSpeed=100;
-	//Adds a cooldown in seconds  for when we need to not auto update camera to velocity 
+UCLASS(ClassGroup = Camera, meta = (BlueprintSpawnableComponent), hideCategories = (Mobility))
+class MODULARMOVEMENT_API UVehicleSpringArm : public USceneComponent
+{
+	GENERATED_UCLASS_BODY()
+	/** Natural length of the spring arm when there are no collisions */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Camera)
+	float TargetArmLength;
+
+	/** offset at end of spring arm; use this instead of the relative offset of the attached component to ensure the line trace works as desired */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Camera)
+	FVector SocketOffset;
+
+	/** Offset at start of spring, applied in world space. Use this if you want a world-space offset from the parent component instead of the usual relative-space offset. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Camera)
+	FVector TargetOffset;
+
+	/** How big should the query probe sphere be (in unreal units) */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = CameraCollision, meta = (editcondition = "bDoCollisionTest"))
+	float ProbeSize;
+
+	/** Collision channel of the query probe (defaults to ECC_Camera) */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = CameraCollision, meta = (editcondition = "bDoCollisionTest"))
+	TEnumAsByte<ECollisionChannel> ProbeChannel;
+
+	/** If true, do a collision test using ProbeChannel and ProbeSize to prevent camera clipping into level.  */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = CameraCollision)
+	uint32 bDoCollisionTest : 1;
+
+	/**
+	 * If this component is placed on a pawn, should it use the view/control rotation of the pawn where possible?
+	 * When disabled, the component will revert to using the stored RelativeRotation of the component.
+	 * Note that this component itself does not rotate, but instead maintains its relative rotation to its parent as normal,
+	 * and just repositions and rotates its children as desired by the inherited rotation settings. Use GetTargetRotation()
+	 * if you want the rotation target based on all the settings (UsePawnControlRotation, InheritPitch, etc).
+	 *
+	 * @see GetTargetRotation(), APawn::GetViewRotation()
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = CameraSettings)
+	uint32 bUsePawnControlRotation : 1;
+
+	/** Should we inherit pitch from parent component. Does nothing if using Absolute Rotation. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = CameraSettings)
+	uint32 bInheritPitch : 1;
+
+	/** Should we inherit yaw from parent component. Does nothing if using Absolute Rotation. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = CameraSettings)
+	uint32 bInheritYaw : 1;
+
+	/** Should we inherit roll from parent component. Does nothing if using Absolute Rotation. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = CameraSettings)
+	uint32 bInheritRoll : 1;
+
+	/**
+	 * If true, camera lags behind target position to smooth its movement.
+	 * @see CameraLagSpeed
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Lag)
+	uint32 bEnableCameraLag : 1;
+
+	/**
+	 * If true, camera lags behind target rotation to smooth its movement.
+	 * @see CameraRotationLagSpeed
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Lag)
+	uint32 bEnableCameraRotationLag : 1;
+
+	/**
+	 * If bUseCameraLagSubstepping is true, sub-step camera damping so that it handles fluctuating frame rates well (though this comes at a cost).
+	 * @see CameraLagMaxTimeStep
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Lag, AdvancedDisplay)
+	uint32 bUseCameraLagSubstepping : 1;
+
+	/**
+	 * If true and camera location lag is enabled, draws markers at the camera target (in green) and the lagged position (in yellow).
+	 * A line is drawn between the two locations, in green normally but in red if the distance to the lag target has been clamped (by CameraLagMaxDistance).
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Lag)
+	uint32 bDrawDebugLagMarkers : 1;
+
+	/** If bEnableCameraLag is true, controls how quickly camera reaches target position. Low values are slower (more lag), high values are faster (less lag), while zero is instant (no lag). */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Lag,
+		meta = (editcondition = "bEnableCameraLag", ClampMin = "0.0", ClampMax = "1000.0", UIMin = "0.0", UIMax =
+			"1000.0"))
+	float CameraLagZSpeed = 10;
+
+
+	/** If bEnableCameraRotationLag is true, controls how quickly camera reaches target position. Low values are slower (more lag), high values are faster (less lag), while zero is instant (no lag). */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Lag,
+		meta = (editcondition = "bEnableCameraRotationLag", ClampMin = "0.0", ClampMax = "1000.0", UIMin = "0.0", UIMax
+			= "1000.0"))
+	float CameraRotationLagSpeed;
+
+	/** Max time step used when sub-stepping camera lag. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Lag, AdvancedDisplay,
+		meta = (editcondition = "bUseCameraLagSubstepping", ClampMin = "0.005", ClampMax = "0.5", UIMin = "0.005", UIMax
+			= "0.5"))
+	float CameraLagMaxTimeStep;
+
+	/** Max distance the camera target may lag behind the current location. If set to zero, no max distance is enforced. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Lag,
+		meta = (editcondition = "bEnableCameraLag", ClampMin = "0.0", UIMin = "0.0"))
+	float CameraLagMaxDistance;
+
+	/**
+	 * Get the target rotation we inherit, used as the base target for the boom rotation.
+	 * This is derived from attachment to our parent and considering the UsePawnControlRotation and absolute rotation flags.
+	 */
+	UFUNCTION(BlueprintCallable, Category = SpringArm)
+	FRotator GetTargetRotation() const;
+
+	/** Get the position where the camera should be without applying the Collision Test displacement */
+	UFUNCTION(BlueprintCallable, Category = CameraCollision)
+	FVector GetUnfixedCameraPosition() const;
+
+	/** Is the Collision Test displacement being applied? */
+	UFUNCTION(BlueprintCallable, Category = CameraCollision)
+	bool IsCollisionFixApplied() const;
+
+	/** Temporary variables when applying Collision Test displacement to notify if its being applied and by how much */
+	bool bIsCameraFixed = false;
+	FVector UnfixedCameraPosition;
+
+	/** Temporary variables when using camera lag, to record previous camera position */
+	FVector PreviousDesiredLoc;
+	FVector PreviousArmOrigin;
+	/** Temporary variable for lagging camera rotation, for previous rotation */
+	FRotator PreviousDesiredRot;
+
+	// UActorComponent interface
+	virtual void OnRegister() override;
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void PostLoad() override;
+	virtual void ApplyWorldOffset(const FVector& InOffset, bool bWorldShift) override;
+	// End of UActorComponent interface
+
+	// USceneComponent interface
+	virtual bool HasAnySockets() const override;
+	virtual FTransform
+	GetSocketTransform(FName InSocketName, ERelativeTransformSpace TransformSpace = RTS_World) const override;
+	virtual void QuerySupportedSockets(TArray<FComponentSocketDescription>& OutSockets) const override;
+	// End of USceneComponent interface
+
+	/** The name of the socket at the end of the spring arm (looking back towards the spring arm origin) */
+	static const FName SocketName;
+
+	/** Returns the desired rotation for the spring arm, before the rotation constraints such as bInheritPitch etc are enforced. */
+	virtual FRotator GetDesiredRotation() const;
+
+protected:
+	/** Cached component-space socket location */
+	FVector RelativeSocketLocation;
+	/** Cached component-space socket rotation */
+	FQuat RelativeSocketRotation;
+
+protected:
+	/** Updates the desired arm location, calling BlendLocations to do the actual blending if a trace is done */
+	virtual void UpdateDesiredArmLocation(bool bDoTrace, bool bDoLocationLag, bool bDoRotationLag, float DeltaTime);
+
+	/**
+	 * This function allows subclasses to blend the trace hit location with the desired arm location;
+	 * by default it returns bHitSomething ? TraceHitLocation : DesiredArmLocation
+	 */
+	virtual FVector BlendLocations(const FVector& DesiredArmLocation, const FVector& TraceHitLocation,
+	                               bool bHitSomething, float DeltaTime);
+
+	//Vehicle spring arm additions
+public :
+	//When input recevied do pause auto camera movement and reduce lag
 	UFUNCTION(BlueprintCallable)
 	void SetCooldown(float In);
 
-	private:
-	float CoolDown;
-	FVector PreviousVelocity;
+	//Speed when auto correct starts ( auto correct rotates camera to vehicle movement direction)
+	UPROPERTY(EditAnyWhere)
+	float AutoCorrectMinSpeedRange = 300;
+	//Speed that Auto Correct reaches its maximum effect
+	UPROPERTY(EditAnyWhere)
+	float AutoCorrectMaxSpeedRange = 3000;
+	//Ignore pitch when auto orienting camera 
+	UPROPERTY(EditAnyWhere)
+	bool IgnorePitch = true;
+	//For smoothing result
+	UPROPERTY(EditAnyWhere)
+	float AutoCorrectInterpolationStrength = 30;
+	//Maximum arm change when accelerating or braking
+	UPROPERTY(EditAnyWhere)
+	float MaxArmLenChange = 200;
+	//Min acceleration ( negative means  braking and shortens the arm len )
+	UPROPERTY(EditAnyWhere)
+	float MinAcceleration = -30;
+	//Max acceleration when arm reaches max len 
+	UPROPERTY(EditAnyWhere)
+	float MaxAcceleration = 20;
+	// Smooth out arm movement 
+	UPROPERTY(EditAnyWhere)
+	float ArmLenInterpolationSpeed = 5;
+	//Min time airborne to play camera shake 
+	UPROPERTY(EditAnyWhere)
+	float MinAirborneTimeForCameraShake = 0.5;
+	//Camera shake to play 
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UCameraShakeBase> CameraShake;
+
+private:
+	//Cooldown to let user handle spring arm manually
+	float CurrentCooldown;
+	//Speed to calculate acceleration
+	float PreviousSpeed;
+	//Curent arm len for interpolation
+	float CurrentArmLen;
+	//To keep track of airborne time
+	float AirborneTime = 0.f;
 };
